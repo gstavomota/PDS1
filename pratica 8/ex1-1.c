631909963b5cb19e931bb8dee0a4b626e2b56e12
Gustavo Lima Mota <61586814+gstavomota@users.noreply.github.com>
2022-07-11 16:53:13 -0300

    Add files via upload

--- pratica 8/ex1-1.c
+++ pratica 8/ex1-1.c
@@ -5,6 +5,16 @@ quanto na vertical, em uma matriz m x n. Depois, você deve
 substituir esses números por zeros e colocá-los nas primeiras 
 inhas da matriz. Todo o programa será implementado a partir das 
 questões a seguir.
+
+1.1) Faça um programa para preencher uma matriz m x n com números aleatórios entre 1 e k. Os valores de m, n e k devem ser lidos do teclado. Como ainda não aprendemos alocação dinâmica de memória, crie uma matriz estaticamente com os limites superiores de m e n. Considere que m e n não podem ser maiores que 100. Não permita que o usuário entre com valores inválidos para m, n e k.
+Exemplo de uma matriz para m=5, n=4 e k=3:
+
+3 3 3 2
+3 2 2 3
+1 1 1 1
+2 1 2 1
+2 3 3 1
+
 */
 #include <stdio.h>
 #include <stdlib.h>
